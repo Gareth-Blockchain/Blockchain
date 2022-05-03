e2eaa6d86d05b7f4e7e7b30accf53ac75e6b5570
Tamara <tamara@linumlabs.com>
2022-05-03 13:59:42 +0200

    Imported ReentrancyGuard.sol

--- Contracts/Treasury.sol
+++ Contracts/Treasury.sol
@@ -1,6 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol"; 
+
 contract Treasury {
 
 }
